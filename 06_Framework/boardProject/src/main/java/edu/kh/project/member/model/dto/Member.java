ab8e7a9576a1d1751db7d262fd8060bf2724a841
ji-hwan <lutungerst@gmail.com>
2023-04-26 16:04:25 +0900

    230426_로그인기능설정진행...

---
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/member/model/dto/Member.java
@@ -0,0 +1,23 @@
+package edu.kh.project.member.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor // 기본 생성자,, 근데 자바에서 기본적으로 제공해주니까.. 굳이?
+@Getter
+@Setter
+@ToString
+public class Member {
+	private int memberNo;
+	private String memberEmail;
+	private String memberPw;
+	private String memberNickname;
+	private String memberTel;
+	private String memberAddress;
+	private String profileImage;
+	private String enrollDate;
+	private String memberDeleteFlag;
+	private int authority;
+}
