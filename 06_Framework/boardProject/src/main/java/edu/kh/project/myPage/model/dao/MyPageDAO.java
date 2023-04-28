0cefcbe3da868dd12707d80f7b762e8ddf19f3f6
ji-hwan <lutungerst@gmail.com>
2023-04-28 17:03:06 +0900

    230428 mypage 내프로필 구현

--- 06_Framework/boardProject/src/main/java/edu/kh/project/myPage/model/dao/MyPageDAO.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/myPage/model/dao/MyPageDAO.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import edu.kh.project.member.model.dto.Member;
+
 @Repository // 저장소(DB)와 관련된 클래스 + Bean 등록 (IOC, 스프링이 객체 관리)
 public class MyPageDAO {
 	
@@ -11,6 +13,15 @@ public class MyPageDAO {
 	// -> root-context.xml에 <bean> 작성됨
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	/** 회원 정보 수정 DAO
+	 * @param updateMember
+	 * @return result
+	 */
+	public int updateInfo(Member updateMember) {
+//		return sqlSession.update("namespace.id", 전달할 값);
+		return sqlSession.update("myPageMapper.updateInfo", updateMember);
+	}
 	
 	
 }
