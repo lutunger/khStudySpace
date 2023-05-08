bae2b77d0fb461462ea0689885e75a6edc92a830
ji-hwan <lutungerst@gmail.com>
2023-05-08 13:51:13 +0900

    230508_회원기능 중간 구현

---
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/board/model/dao/BoardDAO.java
@@ -0,0 +1,25 @@
+package edu.kh.project.board.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAO {
+	
+	// 마이바티스 객체 (root-context.xml 에서 bean 등록)
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	
+	/** 게시판 종류 목록 조회
+	 * @return boardTypeList
+	 */
+	public List<Map<String, Object>> selectBoardTypeList() {
+		return sqlSession.selectList("boardMapper.selectBoardTypeList");
+	};
+	
+}
