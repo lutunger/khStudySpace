b74e615b63f1dc537c933354f725a261b3b32c16
ji-hwan <lutungerst@gmail.com>
2023-05-18 12:48:58 +0900

    230518_스케쥴러기능(예약기능)

--- 06_Framework/boardProject/src/main/java/edu/kh/project/board/model/service/BoardService.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/board/model/service/BoardService.java
@@ -55,6 +55,12 @@ public interface BoardService {
 	List<Map<String, Object>> headerSearch(String query);
 
 	
+	/** DB 이미지(파일 목록 조회)
+	 * @return list
+	 */
+	List<String> selectImageList();
+
+	
 
 	
 }
