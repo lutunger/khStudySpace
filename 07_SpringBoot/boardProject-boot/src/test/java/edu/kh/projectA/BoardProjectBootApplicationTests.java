d38210d96abafe89cf544064e5518d7614f0cc93
ji-hwan <lutungerst@gmail.com>
2023-06-05 09:20:26 +0900

    230602 springboot setup

---
+++ 07_SpringBoot/boardProject-boot/src/test/java/edu/kh/projectA/BoardProjectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.projectA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardProjectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
