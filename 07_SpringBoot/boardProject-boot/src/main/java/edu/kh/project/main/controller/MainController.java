7c5ba18b725b751fae3a523704cfe7cc7a40870d
ji-hwan <lutungerst@gmail.com>
2023-06-09 13:55:01 +0900

    230609_mypage

--- 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -2,7 +2,9 @@ package edu.kh.project.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -21,4 +23,27 @@ public class MainController {
 		
 		return "common/main";
 	}
+	
+	@GetMapping("/loginError")
+	public String loginError(RedirectAttributes ra) {
+		
+		ra.addAttribute("message", "로그인 후 이용해주세요.");
+		
+		return "redirect:/";
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
